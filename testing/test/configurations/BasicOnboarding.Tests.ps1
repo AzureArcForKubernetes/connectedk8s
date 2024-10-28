f186ab9c13b4d38140478678180a4c8be6346fe7
Bavneet Singh <bavneetsingh@microsoft.com>
2024-10-28 11:12:53 -0700

    modify test for asic onboarding

--- testing/test/configurations/BasicOnboarding.Tests.ps1
+++ testing/test/configurations/BasicOnboarding.Tests.ps1
@@ -13,9 +13,36 @@ Describe 'Basic Onboarding Scenario' {
         do 
         {
             $output = az connectedk8s show -n $ENVCONFIG.arcClusterName -g $ENVCONFIG.resourceGroup
+            $jsonOutput = [System.Text.Json.JsonDocument]::Parse($output)
             $provisioningState = ($output | ConvertFrom-Json).provisioningState
+            $autoUpdate = $jsonOutput.RootElement.GetProperty("arcAgentProfile").GetProperty("autoUpdate").GetString()
             Write-Host "Provisioning State: $provisioningState"
-            if ($provisioningState -eq $SUCCEEDED) {
+            Write-Host "Auto Update: $autoUpdate"
+            if ($provisioningState -eq $SUCCEEDED -and $autoUpdate -eq "Enabled") {
+                break
+            }
+            Start-Sleep -Seconds 10
+            $n += 1
+        } while ($n -le $MAX_RETRY_ATTEMPTS)
+        $n | Should -BeLessOrEqual $MAX_RETRY_ATTEMPTS
+    }
+
+    It 'Disable auto-upgrade' {
+        az connectedk8s update -n $ENVCONFIG.arcClusterName -g $ENVCONFIG.resourceGroup --auto-update false
+        $? | Should -BeTrue
+        Start-Sleep -Seconds 10
+
+        # Loop and retry until the configuration installs
+        $n = 0
+        do 
+        {
+            $output = az connectedk8s show -n $ENVCONFIG.arcClusterName -g $ENVCONFIG.resourceGroup
+            $jsonOutput = [System.Text.Json.JsonDocument]::Parse($output)
+            $provisioningState = ($output | ConvertFrom-Json).provisioningState
+            $autoUpdate = $jsonOutput.RootElement.GetProperty("arcAgentProfile").GetProperty("autoUpdate").GetString()
+            Write-Host "Provisioning State: $provisioningState"
+            Write-Host "Auto Update: $autoUpdate"
+            if ($provisioningState -eq $SUCCEEDED -and $autoUpdate -eq "Disabled") {
                 break
             }
             Start-Sleep -Seconds 10
